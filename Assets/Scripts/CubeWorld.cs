c80ffdcbb61ddfe08bd68398393794fb50f24327
pgermon <pgermon.pro@gmail.com>
2020-12-18 12:42:12 +0100

    création dossiers et test obstacles Perlin

--- Assets/Scripts/CubeWorld.cs
+++ Assets/Scripts/CubeWorld.cs
@@ -13,6 +13,7 @@ public class CubeWorld : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+		GameObject surface_go = Instantiate(surface, new Vector3(0f, 0f, 0f), Quaternion.identity);
 		Transform surface_t = surface.transform;
 		Vector3 surface_dimensions = surface_t.lossyScale;
 
