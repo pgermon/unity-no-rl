7271e018b57796d66dda68ee4c956061bc38be81
Benjamin Le Rohellec <benjamin@lerohellec.nom.fr>
2021-01-06 19:31:56 +0100

    correction versionnement
    Il faut partager les fichier `.meta` car ils fournissent des identifiants uniques associés aux fichiers.

--- Assets/Scripts/CubeWorld.cs
+++ Assets/Scripts/CubeWorld.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeWorld : MonoBehaviour
@@ -13,18 +11,18 @@ public class CubeWorld : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		GameObject surface_go = Instantiate(surface, new Vector3(0f, 0f, 0f), Quaternion.identity);
+		// GameObject surface_go = Instantiate(surface, new Vector3(0f, 0f, 0f), Quaternion.identity);
 		Transform surface_t = surface.transform;
 		Vector3 surface_dimensions = surface_t.lossyScale;
 
-		Perlin2DMap perlin = new Perlin2DMap(0.2f, 0.2f, 6f);
+		Perlin2DMap perlin = new Perlin2DMap(0.1f, 0.4f, 4f);
 
-		for (int x = (int) Mathf.Floor(-surface.transform.localScale.x * 5); x < (int) Math.Ceiling(surface.transform.localScale.x * 5); x++)
-			for (int z = (int) Mathf.Floor(-surface.transform.localScale.z * 5); z < (int) Math.Ceiling(surface.transform.localScale.z * 5); z++) {
-				float height = (float) Math.Floor(perlin.at(x, z) - 1 + Mathf.Sqrt((x * x + z * z)) / 8);
+		for (int x = (int) Mathf.Floor(-surface_dimensions.x * 5); x < (int) Math.Ceiling(surface_dimensions.x * 5); x++)
+			for (int z = (int) Mathf.Floor(-surface_dimensions.z * 5); z < (int) Math.Ceiling(surface_dimensions.z * 5); z++) {
+				float height = (float) Math.Floor(perlin.at(x, z));
 				if (height >= 1) {
 					GameObject obstacle = GameObject.Instantiate(cube);
-					obstacle.transform.position = surface.transform.position + new Vector3(x, 0, z);
+					obstacle.transform.position = surface_t.position + new Vector3(x, 0, z);
 					obstacle.transform.localScale = new Vector3(1f, height, 1f);
 					obstacle.transform.position += new Vector3(0.5f, height / 2, 0.5f);
 				}
