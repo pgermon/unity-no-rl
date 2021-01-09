85bfb2bd443537db929aab703efda03c4c137bd5
CharlyENSC <45589099+CharlyENSC@users.noreply.github.com>
2021-01-09 18:21:18 +0100

    Last

--- Assets/Scripts/CubeWorld.cs
+++ Assets/Scripts/CubeWorld.cs
@@ -17,15 +17,16 @@ public class CubeWorld : MonoBehaviour
 		Vector3 surface_dimensions = surface_t.lossyScale;
 
 		Perlin2DMap perlin = new Perlin2DMap(0.1f, 0.4f, 4f);
-
 		for (int x = (int) Mathf.Floor(-surface_dimensions.x * 5); x < (int) Math.Ceiling(surface_dimensions.x * 5); x++)
 			for (int z = (int) Mathf.Floor(-surface_dimensions.z * 5); z < (int) Math.Ceiling(surface_dimensions.z * 5); z++) {
-				float height = (float) Math.Floor(perlin.at(x, z));
+                float noise = UnityEngine.Random.Range(0f, 2f);
+
+                float height = (float) Math.Floor(perlin.at(x, z));
 				if (height >= 1) {
 					GameObject obstacle = GameObject.Instantiate(cube);
 					obstacle.transform.position = surface_t.position + new Vector3(x, 0, z);
-					obstacle.transform.localScale = new Vector3(1f, height, 1f);
-					obstacle.transform.position += new Vector3(0.5f, height / 2, 0.5f);
+					obstacle.transform.localScale = new Vector3(1f, height-noise, 1f);
+					obstacle.transform.position += new Vector3(0.5f+noise, 0, 0.5f+noise);
 				}
 			}
 	}
