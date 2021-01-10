b4f2181fcff48033a27dc59f127ca47e4df12e5d
CharlyENSC <45589099+CharlyENSC@users.noreply.github.com>
2021-01-10 14:14:57 +0100

    relief

---
+++ Assets/TreeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        Debug.Log(coll.gameObject.name);
+        if (coll.gameObject.name != "GameTerrain")
+        {
+            Destroy(this.gameObject);
+        }
+
+     
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
