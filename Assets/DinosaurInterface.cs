29d3381765532f36eb92bafc3c072df0e9b4abf5
Hugo MARTIN <hugo.martin.info@gmail.com>
2021-01-10 15:56:31 +0100

    Dino Interface & debut TRex

---
+++ Assets/DinosaurInterface.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface DinosaurInterface
+{
+    void runTo(Vector3 position);
+    void die();
+    //Calls growUp is the target dies
+    void attack(DinosaurInterface target);
+    void growUp();
+}
