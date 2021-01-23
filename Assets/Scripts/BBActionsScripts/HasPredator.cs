0182ff8646011bd6a17ce52cc61960c833399b1b
Benjamin Le Rohellec <benjamin@lerohellec.nom.fr>
2021-01-23 12:33:13 +0100

    BB: ajout `HasPredator` condition

--- Assets/Scripts/BBActionsScripts/HasPredator.cs
+++ Assets/Scripts/BBActionsScripts/HasPredator.cs
@@ -1,21 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 using Pada1.BBCore;           // Code attributes
-using Pada1.BBCore.Tasks;     // TaskStatus
-using Pada1.BBCore.Framework; // BasePrimitiveAction
 using BBUnity.Conditions;
 
-[System.Obsolete("-> HasPredator")]
-[Condition("Behaviors/DinoConditions/IsPredatorNear")]
-[Help("Returns True if the currentPredator of DinosaurBB is not null")]
-
-public class IsPredatorNear : GOCondition 
+[Condition("Behaviors/DinoConditions/HasPredator")]
+[Help("Search a predator and return true if found")]
+public class HasPredator : GOCondition 
 {
     [OutParam("predator")]
+    [Help("The predator found")]
     public GameObject current_predator;
-
+    
     public override bool Check(){
         current_predator = gameObject.GetComponent<DinosaurBB>().getCurrentPredator();
         bool ret = current_predator != null;
